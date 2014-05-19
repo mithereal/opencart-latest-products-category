28b633d525795438dd12fb5c511f0e05abcc7253
Jason <mithereal@gmail.com>
2014-05-18 21:52:10 -0700

    initial commit

---
+++ upload/admin/language/english/module/pdf_catalog.php
@@ -0,0 +1,41 @@
+<?php
+// Heading
+$_['heading_title']    = 'PDF Catalog';
+
+// Text
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified module PDF catalog!';
+$_['text_hide']       = 'Hide';
+$_['text_description']       = 'Description';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+$_['text_yes'] = 'Yes';
+$_['text_no'] = 'No';
+$_['text_native'] = 'Native';
+
+// Entry
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']   = 'Position:';
+$_['entry_status']     = 'Status:';
+$_['entry_display_toc'] = 'Display Toc:';
+$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_description'] = 'Introduction in PDF:';
+$_['entry_display_categories'] = 'Display categories:';
+$_['entry_pdf_creator'] = 'Creator:';
+$_['entry_pdf_author'] = 'Author:';
+$_['entry_pdf_title'] = 'Title:';
+$_['entry_pdf_max_products'] = 'Max Products:';
+$_['entry_pdf_subject'] = 'Subject:';
+$_['entry_pdf_keywords'] = 'Keywords:';
+$_['entry_pdf_catalog_display_description'] = 'Display Item Description:';
+$_['entry_pdf_catalog_image_height'] = 'Image Height:';
+$_['entry_pdf_catalog_image_width'] = 'Image Width:';
+$_['entry_pdf_catalog_item_per_page'] = 'Max Images Per Page:';
+$_['entry_pdf_catalog_description_chars'] = 'Max Chars in Description:';
+$_['entry_pdf_catalog_template_type'] = 'Template Type:';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify module PDF catalog!';
+?>
